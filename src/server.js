1e578c789c7b7bd7f220b6bd3d27da2b86b8c3d7
shiauke <shiauke@hotmail.com>
2025-07-13 18:01:50 +0800

    modify to test version

--- src/server.js
+++ src/server.js
@@ -6,11 +6,20 @@ const fs = require('fs');
 
 app.use(express.static('../public'));
 
-app.get('/excels/:file', (req, res) => {
-    // const fixedFileName = 'result_formatted.json';
-    // const fileName = fixedFileName; // 使用固定的檔案名稱for測試
+// app.get('/excels/:file', (req, res) => {
+//     const fileName = req.params.file;
+//     const filePath = path.join(__dirname, 'excels', fileName);
+//     if (fs.existsSync(filePath)) {
+//         const fileContent = fs.readFileSync(filePath, 'utf8');
+//         res.setHeader('Content-Type', 'application/json');
+//         res.send(fileContent);
+//     } else {
+//         res.status(404).send({ error: 'File not found' });
+//     }
+// });
+app.get('/test/:file', (req, res) => {
     const fileName = req.params.file;
-    const filePath = path.join(__dirname, 'excels', fileName);
+    const filePath = path.join(path.dirname(__dirname), 'test', fileName);
     if (fs.existsSync(filePath)) {
         const fileContent = fs.readFileSync(filePath, 'utf8');
         res.setHeader('Content-Type', 'application/json');

87666d78fb012b2af75a376af24ad1f3d427ccd6
shiauke <shiauke@hotmail.com>
2025-07-13 16:38:39 +0800

    remove unused modules

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const fs = require('fs');
+
+app.use(express.static('public'));
+
+
+app.get('/excels/:file', (req, res) => {
+    // const fixedFileName = 'result_formatted.json';
+    // const fileName = fixedFileName; // 使用固定的檔案名稱for測試
+    const fileName = req.params.file;
+    const filePath = path.join(__dirname, 'excels', fileName);
+    if (fs.existsSync(filePath)) {
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+        res.setHeader('Content-Type', 'application/json');
+        res.send(fileContent);
+    } else {
+        res.status(404).send({ error: 'File not found' });
+    }
+});
+
+
+app.get('/', (req, res) => {    
+    res.send('Hello, world!');
+});
+
+
+
+app.listen(port, async () => {
+  console.log(`Server running at http://localhost:${port}`);
+});

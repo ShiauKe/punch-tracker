73ba2e82430310f62b5eb3a20af41f83439d2d09
shiauke <shiauke@hotmail.com>
2025-07-14 21:57:18 +0800

    punchRule.js is used to maintain the standard punch time

---
+++ src/excels/rules/punchRule.js
@@ -0,0 +1,9 @@
+// punchRule.js
+module.exports = {
+    mi: { start: '07:30:00', center:'08:00:00', end: '08:30:00' },
+    mo: { start: '11:30:00', center:'12:00:00', end: '12:30:00' },
+    ai: { start: '12:30:00', center:'13:00:00', end: '13:30:00' },
+    ao: { start: '16:30:00', center:'17:00:00', end: '17:30:00', aux_fill:'18:00:01' }
+}
+
+
